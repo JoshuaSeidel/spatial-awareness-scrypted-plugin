19245bde4632b2e97d8910a8c6d2f7936d519dfe
Joshua Seidel <29733828+JoshuaSeidel@users.noreply.github.com>
2026-01-01 19:24:13 -0500

    chore: Remove node_modules/dist/out from tracking

--- node_modules/@scrypted/common/src/clone-deep.ts
+++
@@ -1,5 +0,0 @@
-export function cloneDeep(o: any) {
-    if (!o)
-        return;
-    return JSON.parse(JSON.stringify(o));
-}
--- node_modules/@scrypted/common/src/resolution-utils.ts
+++
@@ -1,4 +0,0 @@
-export function fitHeightToWidth(actualWidth: number, actualHeight: number, requestedWidth: number) {
-    const h = Math.round((actualHeight / actualWidth) * requestedWidth);
-    return h;
-}
